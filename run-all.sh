26aa19f5efa7b94ca5aed27d491f12ed4fb1d219
Aasim Khan <aasimkhan30@gmail.com>
2019-11-26 09:56:09 -0800

    Added fixed parameters in run-all.sh

--- run-all.sh
+++ run-all.sh
@@ -1,3 +1,30 @@
-cp -r ~/projs/esesc/conf/* ./
-cp -r ~/projs/esesc/bins/* ./
-riscv64-linux-gnu-g++ ./examples/Robot1/main.cpp -I  ./include/  -static -o3
+#!/bin/bash
+riscv64-linux-gnu-g++ ./examples/Robot1/main.cpp -I  ./include/  -static -o3 -o kalman.rv
+export ESESC_BenchName='./kalman.rv'
+
+case "$1" in
+	-fixed)
+		echo "---------------------------"
+		echo "Running Emulator with FIXES"
+		echo "---------------------------"
+		export ESESC_IL1_core_bsize=64
+		export ESESC_IL1_core_assoc=8
+		export ESESC_PrivL2_bsize=128
+		export ESESC_PrivL2_assoc=16
+		break
+		;;
+	-fixedl3)
+		echo "---------------------------"
+		echo "Running Emulator with FIXES and adding L3 cache"
+		echo "---------------------------"
+		export ESESC_IL1_core_bsize=64
+		export ESESC_IL1_core_assoc=8
+		export ESESC_PrivL2_bsize=128
+		export ESESC_PrivL2_assoc=16
+		export ESESC_PrivL2_lowerLevel="L3Cache L3 shared"
+		break
+		;;
+	esac
+
+../main/esesc
+./scripts/report.pl -last
